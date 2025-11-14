84f7b77f9a98f84dd4329100bcff3db4b0ffccca
Krzysztof <krzysztof@engineer.com>
2025-11-14 14:45:42 +0100

    add static assets

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,15 @@ package router
 import (
 	"net/http"
 	"scaffold/internal/handler"
+	"scaffold/static"
 )
 
 func New() *http.ServeMux {
 	r := http.NewServeMux()
 
 	r.HandleFunc("/", handler.Home())
+
+	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(static.FS))))
 
 	return r
 }
